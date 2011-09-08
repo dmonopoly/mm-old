88e551c2293fca2151524495fe5830104fa9a27f
David Zhang <dzhang21@gmail.com>
2011-09-08 00:30:49 -0700

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Memorymap::Application.config.secret_token = 'c25f8151e5ef5c5f8ef6e44b9520e489bc2b1b38dcf4ceb97db69442c22e7913a6f123996bccbe862427baa2247028c36270188fb0b4187f39636defe3234f7d'
