9db451ad803d6a39a166f83c022a3b73f4df3624
David Zhang <dzhang21@gmail.com>
2011-12-08 16:52:31 -0800

    refined interface more - more buttons, fixed char indicator with &nbsp;

--- app/assets/javascripts/memories.js.coffee
+++ app/assets/javascripts/memories.js.coffee
@@ -16,7 +16,7 @@ jQuery ->
     text = $('textarea#content_text_area').val() # store text of textarea
     $("#ghost_content_area").html(text)
     if text.length > 140
-      $("#ghost_content_area").css('color', 'red') # change color of ghost content
+      $("#ghost_content_area").css('color','red') # change color of ghost content
       $("#submit_memory_button").attr('disabled','disabled') # disable submit button
     else
       $("#ghost_content_area").css('color', proper_color)
@@ -24,9 +24,13 @@ jQuery ->
     
     # Character indicator appears if user has <= 10 characters left
     if text.length > 129
+      # $("#character_indicator").css('visibility','visible')
+      # $("#character_indicator").css('display','block')
       $("#character_indicator").html(140-text.length)
     else
-      $("#character_indicator").html('')
+      # $("#character_indicator").css('visibility','hidden')
+      # $("#character_indicator").css('display','none')
+      $("#character_indicator").html('&nbsp;')
     
 	# Triggers for superadmin users list:
 	# Superadmin
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,21 +10,21 @@ module ApplicationHelper
   # location - the path to the partial that you want rendered upon calling this. Must end with backslash.
   #   Convention: this partial's name must be like "_page_fields", not "_page_form"
   # element - the element after which the partial will appear (uses the last instance of this element)
-  def link_to_add_fields(name, f, association, location, element)
+  def link_to_add_fields(name, f, association, location, element, html_options={})
     new_object = f.object.class.reflect_on_association(association).klass.new
     fields = f.fields_for(association, new_object, :child_index => "new_#{association}") do |builder|
       render(location + association.to_s.singularize + "_fields", :f => builder)
     end
     # link_to_function(name, h("add_fields(this, '#{association}', '#{escape_javascript(fields)}')"))
     # link_to_function(name, "add_fields(this, '#{association}', '#{escape_javascript(fields)}')")
-    link_to_function(name, "add_fields('#{element}', '#{association}', '#{escape_javascript(fields)}')")
+    link_to_function(name, "add_fields('#{element}', '#{association}', '#{escape_javascript(fields)}')", html_options)
   end
   
   # Creates a link that removes the *previous* field in a complex form
   # Important: place code like the following right after the input field
   # Example: <%= link_to_remove_fields "remove", f, "fields" %>
   # The container of this link_to must have class class_name
-  def link_to_remove_fields(name, f, class_name)
-    f.hidden_field(:_destroy) + link_to_function(name, "remove_fields(this, '#{class_name}')")
+  def link_to_remove_fields(name, f, class_name, html_options={})
+    f.hidden_field(:_destroy) + link_to_function(name, "remove_fields(this, '#{class_name}')", html_options)
   end
 end
